43607139005b43bc1de281e81c45b53305fc11ac
yichao <1793152758@qq.com>
2023-11-22 23:06:53 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         name='lade',
-        version='0.0.1',
+        version='0.0.2',
         description='Lookahead Decoding Implementation',
         author='Fu Yichao',
         author_email='yichaofu2000@outlook.com',
         license='Apache-2',
         url='https://github.com/hao-ai-lab/LookaheadDecoding.git',
-        packages=['lade']
+        packages=['lade', 'lade.models']
     )
 
